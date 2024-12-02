15db0034e7f21e3bb15eceef5698523eaf3b6eeb
Pro5507 <nhupendrakumar9162@gmail.com>
2024-12-02 18:07:31 +0530

    Int to Rome

---
+++ main.py
@@ -0,0 +1,28 @@
+import random
+class Inte():
+    def __init__(self, num):
+        self.num= num
+num= random.randint(1,10)
+class rome():
+    def __init__(self, romen):
+        self.romen= romen
+if num== 1:
+    print("1=I")
+elif num== 2:
+    print("2=II")
+elif num== 3:
+    print("3=III")   
+elif num== 4:
+    print("4=IV")
+elif num== 5:
+    print("5=V")
+elif num== 6:
+    print("6=VI")
+elif num== 7:
+    print("7=VII")
+elif num== 8:
+    print("8=VIII")
+elif num== 9:
+    print("9=IX")
+else:
+    print("10=X")
